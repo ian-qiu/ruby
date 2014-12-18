7e967f05dd329ab0d64ab187ee27c716a1294401
ian-qiu <qiuyiyi2006@163.com>
2014-12-18 14:23:35 +0800

    Update time.rb

--- time.rb
+++ time.rb
@@ -1,4 +1,11 @@
 #!/usr/local/bin/ruby
-require 'time'
-date = '2014-12-16'
-puts Time::parse(date).to_i
+
+# current unix timestamp
+puts Time.now.to_i
+# today
+puts Time.now.strftime('%Y-%m-%d')
+str_date = '2014-12-16'
+unix_timestamp = Time::parse(str_date).to_i
+puts unix_timestamp
+str_date = Time.at(unix_timestamp).strftime('%Y-%m-%d')
+puts str_date
